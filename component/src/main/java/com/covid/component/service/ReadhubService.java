cc4eaaa27989c35c5ea764dc133b182c7863932c
BaiYang <baiyang@qufaya.com>
2022-07-28 14:19:23 +0800

    method to refresh Readhub news

--- component/src/main/java/com/covid/component/service/ReadhubService.java
+++ component/src/main/java/com/covid/component/service/ReadhubService.java
@@ -2,12 +2,12 @@ package com.covid.component.service;
 
 import com.covid.component.domain.ReadhubDailyNews;
 import com.covid.component.domain.ReadhubDailyNewsAgg;
+import com.covid.component.domain.ReadhubDailyNewsExample;
 import com.covid.component.mapper.ReadhubDailyNewsAggMapper;
 import com.covid.component.mapper.ReadhubDailyNewsMapper;
 import com.covid.fetcher.ReadhubDailyNewsData;
 import com.covid.fetcher.ReadhubFetcher;
 import com.covid.fetcher.ReadhubNewsAgg;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,29 +29,57 @@ public class ReadhubService {
     private ReadhubDailyNewsAggMapper readhubDailyNewsAggMapper;
 
     public void fetchReadhubNewsAndSaveToDb() throws Exception {
+        LocalDateTime now = LocalDateTime.now();
+        if (isNewsFetched(now)) {
+            return;
+        }
+
         List<ReadhubDailyNewsData> readhubDailyNewsDataList = readhubFetcher.fetchReadhubDailyNews();
 
-        LocalDateTime now = LocalDateTime.now();
+        saveAllNewsToDb(now, readhubDailyNewsDataList);
+    }
+
+    private void saveAllNewsToDb(LocalDateTime now, List<ReadhubDailyNewsData> readhubDailyNewsDataList) {
         readhubDailyNewsDataList.forEach(data -> {
-            ReadhubDailyNews dailyNews = new ReadhubDailyNews();
-            dailyNews.setTitle(data.getTitle());
-            dailyNews.setSummary(data.getSummary());
-            dailyNews.setSiteName(data.getSiteNameDisplay());
-            dailyNews.setNewsCreatedAt(data.getNewsCreatedAt());
-            dailyNews.setNewsDate(now.toLocalDate());
-            dailyNews.setCreatedAt(now);
-            readhubDailyNewsMapper.insert(dailyNews);
+            ReadhubDailyNews dailyNews = saveDailyNews(now, data);
 
             List<ReadhubNewsAgg> newsAggList = data.getNewsAggList();
             newsAggList.forEach(agg -> {
-                ReadhubDailyNewsAgg newsAgg = new ReadhubDailyNewsAgg();
-                newsAgg.setNewsId(dailyNews.getId());
-                newsAgg.setTitle(agg.getTitle());
-                newsAgg.setSiteName(agg.getSiteNameDisplay());
-                newsAgg.setUrl(agg.getUrl());
-                newsAgg.setCreatedAt(now);
-                readhubDailyNewsAggMapper.insert(newsAgg);
+                saveNewsAgg(now, dailyNews, agg);
             });
         });
+    }
+
+    private void saveNewsAgg(LocalDateTime now, ReadhubDailyNews dailyNews, ReadhubNewsAgg agg) {
+        ReadhubDailyNewsAgg newsAgg = new ReadhubDailyNewsAgg();
+        newsAgg.setNewsId(dailyNews.getId());
+        newsAgg.setTitle(agg.getTitle());
+        newsAgg.setSiteName(agg.getSiteNameDisplay());
+        newsAgg.setUrl(agg.getUrl());
+        newsAgg.setCreatedAt(now);
+        readhubDailyNewsAggMapper.insert(newsAgg);
+    }
+
+    private ReadhubDailyNews saveDailyNews(LocalDateTime now, ReadhubDailyNewsData data) {
+        ReadhubDailyNews dailyNews = new ReadhubDailyNews();
+        dailyNews.setTitle(data.getTitle());
+        dailyNews.setSummary(data.getSummary());
+        dailyNews.setSiteName(data.getSiteNameDisplay());
+        dailyNews.setNewsCreatedAt(data.getNewsCreatedAt());
+        dailyNews.setNewsDate(now.toLocalDate());
+        dailyNews.setCreatedAt(now);
+        readhubDailyNewsMapper.insert(dailyNews);
+        return dailyNews;
+    }
+
+    private boolean isNewsFetched(LocalDateTime now) {
+        ReadhubDailyNewsExample readhubDailyNewsExample = new ReadhubDailyNewsExample();
+        readhubDailyNewsExample.createCriteria()
+                .andNewsDateEqualTo(now.toLocalDate());
+        long dailyNewsCount = readhubDailyNewsMapper.countByExample(readhubDailyNewsExample);
+        if (dailyNewsCount > 0) {
+            return true;
+        }
+        return false;
     }
 }
--- web/src/main/java/com/covid/web/controller/CovidController.java
+++ web/src/main/java/com/covid/web/controller/CovidController.java
@@ -1,6 +1,7 @@
 package com.covid.web.controller;
 
 import com.covid.component.service.CovidService;
+import com.covid.component.service.ReadhubService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +17,19 @@ public class CovidController {
     @Autowired
     private CovidService covidService;
 
+    @Autowired
+    private ReadhubService readhubService;
+
     @GetMapping("refresh")
     public String refreshCovidData() throws Exception {
         covidService.fetchCovidDataAndSaveToDb();
+
+        return "success";
+    }
+
+    @GetMapping("refresh/readhub")
+    public String refreshReadhubNews() throws Exception {
+        readhubService.fetchReadhubNewsAndSaveToDb();
 
         return "success";
     }
